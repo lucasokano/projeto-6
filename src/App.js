439ee64ce5702ab5a4621bc6ff0de057c694bdbd
lucasokano <luc4sko@hotmail.com>
2021-06-24 21:37:41 -0300

    animação

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
+import React from 'react';
 import './App.css';
+import Anima1 from './components/Anima1'
 
 function App() {
   return (
     <div className="App">
-      
+      <Anima1 />
     </div>
   );
 }
 
-export default App;
+export default App
---
+++ src/components/Anima1/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './index.css'
+
+const Anima1 = () => {
+    return(
+        <div className="Anima1">
+            <h1>Animação que pisca</h1>
+            <div className="icones">
+                <i className="fas fa-hotdog"></i>
+                <i className="fas fa-egg"></i>
+                <i className="fas fa-ice-cream"></i>
+            </div>
+        </div>
+    )
+}
+
+export default Anima1
