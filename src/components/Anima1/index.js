02333782c00dc010097f67f14b7f7371be9a2b25
lucasokano <luc4sko@hotmail.com>
2021-06-27 20:11:45 -0300

    animação adicionada

--- src/components/Anima1/index.js
+++ src/components/Anima1/index.js
@@ -1,14 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './index.css'
 
 const Anima1 = () => {
+    
+    const [A, setA] = useState()
+        
+    setInterval(() => {
+        var a = Math.random()*3
+        if(a<1){
+            setA(<i className="fas fa-hotdog"></i>)
+        }
+        else if( a>1 && a<2) {
+            setA(<i className="fas fa-egg"></i>)
+        }
+        else {
+            setA(<i className="fas fa-ice-cream"></i>)
+        }
+    }, 2000);
+      
+
     return(
         <div className="Anima1">
             <h1>Animação que pisca</h1>
             <div className="icones">
-                <i className="fas fa-hotdog"></i>
-                <i className="fas fa-egg"></i>
-                <i className="fas fa-ice-cream"></i>
+                {A}
             </div>
         </div>
     )
